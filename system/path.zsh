a52628681c01f6d476023d1654dd1685db2f3611
Alex Ganose <alexganose@googlemail.com>
2012-10-22 10:11:01 +0100

    Add /usr/local/bin to $PATH to work with `brew` Convert some vundle plugin directories to git submodules so they are processes properly by git.
    Minor vim and zsh changes.
    
    In tmux rebind C-b modifier to C-a

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH=/Users/alexganose/.rvm/gems/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/gems/ruby-1.9.3-p194@global/bin:/Users/alexganose/.rvm/rubies/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/Users/alexganose/.rvm/gems/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/gems/ruby-1.9.3-p194@global/bin:/Users/alexganose/.rvm/rubies/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/bin
-
+export PATH=" /usr/local/share/npm/bin:$PATH"
 export PATH="./bin:$PATH"
+export PATH="/usr/texbin:$PATH"
+export PATH="/usr/local/bin:$PATH"
