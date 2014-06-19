319794f23f967a90ffb2286607596e59b224f069
Alex Ganose <alexganose@googlemail.com>
2014-06-19 22:44:49 +0100

    Changing from RVM to rbenv

--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,2 @@
-export PATH=/Users/alexganose/.rvm/gems/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/gems/ruby-1.9.3-p194@global/bin:/Users/alexganose/.rvm/rubies/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/Users/alexganose/.rvm/gems/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/gems/ruby-1.9.3-p194@global/bin:/Users/alexganose/.rvm/rubies/ruby-1.9.3-p194/bin:/Users/alexganose/.rvm/bin
-export PATH=" /usr/local/share/npm/bin:$PATH"
-export PATH="./bin:$PATH"
-export PATH="/usr/texbin:$PATH"
-export PATH="/usr/local/bin:$PATH"
-export PATH="/usr/local/sbin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
