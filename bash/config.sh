2cece137fed7c17463f2f5d6aa973dcb036542fb
Alex Ganose <alexganose@googlemail.com>
2018-01-30 14:31:51 +0000

    Update bashrc and config

--- bash/config.sh
+++ bash/config.sh
@@ -1,14 +1,48 @@
 eval `dircolors ${HOME}/.dircolors`
 export CLICOLOR=true
+export GREP_OPTIONS='--color=auto'
+export LS_OPTIONS='--color=auto'
+export EDITOR='vim'
 
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=10000
 SAVEHIST=10000
 
-shopt histverify
-shopt autcd
+shopt -s histverify
+shopt -s autcd
 
-shopt histappend # adds history
+shopt -s histappend # adds history
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-shopt expand_aliases
+shopt -s expand_aliases
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+# makes default folder permission 755
+umask 022
+
