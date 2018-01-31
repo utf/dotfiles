6c38719d31fb220c6105cc01ecfda45575fb0801
Alex Ganose <alexganose@googlemail.com>
2018-01-31 00:00:38 +0000

    Update system path

--- system/path.sh
+++ system/path.sh
@@ -2,9 +2,11 @@ if [[ "${cluster}" == "mbp" ]]; then
   export PATH="${HOME}/bin:/usr/local/bin:/usr/local/sbin:$DPATH/bin:$PATH"
   export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 elif [[ "${cluster}" == "ucl" ]]; then
-  export PATH=${HOME}/bin/tools:${HOME}/bin/boltz:${PATH}
-  export PYTHONPATH=${HOME}/bin/sauce:${PYTHONPATH}
-  export VASP_PP_PATH="${HOME}/bin/cookbook/ase-potcars"
-  export VASP_SCRIPT="${HOME}/bin/sauce/scripts/run-vasp.py"
-  export FW_CONFIG_FILE=${HOME}/work/p999_atomate/atomate/config/FW_config.yaml
+  export PATH="${HOME}/bin/tools:${HOME}/src/vaspup/bin:${PATH}"
+  export PYTHONPATH=${HOME}/src/sauce:${PYTHONPATH}
+  export VASP_PP_PATH="${HOME}/src/cookbook/ase-potcars"
+  export VASP_SCRIPT="${HOME}/src/sauce/scripts/run-vasp.py"
+
+  export VASP_POTENTIALS="${HOME}/src/potentials_PBE.54" # required by vaspup
+  export FW_CONFIG_FILE="${HOME}/work/p999_atomate/atomate/config/FW_config.yaml"
 fi
