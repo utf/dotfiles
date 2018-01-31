0acbd1bd68e694f3b3ddf4e83b3e3fcba32c6728
Alex Ganose <alexganose@googlemail.com>
2018-01-31 00:07:03 +0000

    Start using python3 by default

--- system/clusters.sh
+++ system/clusters.sh
@@ -2,5 +2,5 @@ if [[ "${cluster}" == "ucl" ]]; then
   export TERM="xterm"
   export NCORE=8
   source /shared/ucl/apps/bin/defmods
-  module load python2 grace ghostscript texlive ruby
+  module load python3 grace ghostscript texlive ruby
 fi
