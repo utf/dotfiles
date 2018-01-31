37c470c728e50eded77d39e9312c090b50457a9a
Alex Ganose <alexganose@googlemail.com>
2018-01-31 10:54:57 +0000

    Update potentials directories

--- system/path.sh
+++ system/path.sh
@@ -4,9 +4,9 @@ if [[ "${cluster}" == "mbp" ]]; then
 elif [[ "${cluster}" == "ucl" ]]; then
   export PATH="${HOME}/bin/tools:${HOME}/src/vaspup/bin:${PATH}"
   export PYTHONPATH=${HOME}/src/sauce:${PYTHONPATH}
-  export VASP_PP_PATH="${HOME}/src/cookbook/ase-potcars"
+  export VASP_PP_PATH="${HOME}/src/potentials/ase"
   export VASP_SCRIPT="${HOME}/src/sauce/scripts/run-vasp.py"
 
-  export VASP_POTENTIALS="${HOME}/src/potentials_PBE.54" # required by vaspup
+  export VASP_POTENTIALS="${HOME}/src/potentials/potpaw_PBE.54" # required by vaspup
   export FW_CONFIG_FILE="${HOME}/work/p999_atomate/atomate/config/FW_config.yaml"
 fi
