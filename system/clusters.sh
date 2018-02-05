01552dff0e8484a57563174567bac0f26b6a56a1
Alex Ganose <alexganose@googlemail.com>
2018-02-05 15:14:36 +0000

    Set DISPLAY on donut

--- system/clusters.sh
+++ system/clusters.sh
@@ -3,4 +3,8 @@ if [[ "${cluster}" == "ucl" ]]; then
   export NCORE=8
   source /shared/ucl/apps/bin/defmods
   module load python2 grace ghostscript texlive ruby
+
+elif [[ "${cluster}" == "donut" ]]; then
+  export DISPLAY="localhost:0.0"
+
 fi
