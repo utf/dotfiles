e643fd6ff07dbca65e5345ce6e066ad6f3f1ec91
Alex Ganose <alexganose@googlemail.com>
2018-02-13 16:14:40 +0000

    Use python3 by default

--- system/clusters.sh
+++ system/clusters.sh
@@ -2,7 +2,7 @@ if [[ "${cluster}" == "ucl" ]]; then
   export TERM="xterm"
   export NCORE=8
   source /shared/ucl/apps/bin/defmods
-  module load python2 grace ghostscript texlive ruby
+  module load python3 grace ghostscript texlive ruby
 
 elif [[ "${cluster}" == "donut" ]]; then
   export DISPLAY="localhost:0.0"
